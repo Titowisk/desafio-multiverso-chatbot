89eea8d424e6a8915935b39b78ff3f0c89408dd7
Vitor Ribeiro <rabelo51@gmail.com>
2018-10-22 17:50:18 -0300

    Add prompt-sync have a conversation with watson assistant through node.js console

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const watson = require('./config.json');
 const bodyParser = require('body-parser');
+var prompt = require('prompt-sync')();
 let AssistantV1 = require('watson-developer-cloud/assistant/v1'); // watson sdk
 
 const assistant = new AssistantV1({
@@ -9,16 +10,36 @@ const assistant = new AssistantV1({
     iam_apikey: watson.apikey,
 });
 
-console.log(assistant); // test
+let processResponse = (err, response) => {
+    if (err) {
+        console.error(err);
+        return;
+    }
 
+    // If an intent was detected, log it out to the console.
+    if (response.intents.length > 0) {
+        console.log(`Detected intent: #${response.intents[0].intent}`);
+    }
+
+    // Display the output from dialog, if any. Assumes a single text response.
+    if (response.output.generic.length != 0) {
+        console.log(response.output.generic[0].text);
+    }
+
+    // Prompt for the next round of input.
+    let newMessageFromUser = prompt('>> ');
+    assistant.message({
+        workspace_id: watson.workspace_id,
+        input: {text: newMessageFromUser}
+    }, processResponse);
+
+};
+
+// Start conversation with empty message.
 assistant.message({
-    workspace_id: watson.workspace_id,
-    input: {'text': "qual o seu nome?"},
-    headers: {'Custom-header': 'custom', 'Accept-Language': 'custom'}
-}, (err, result, response) => {
-    if (err) {console.log('error:', err)}
-    else {console.log(JSON.stringify(result, null, 2))};
-});
+    workspace_id: watson.workspace_id
+}, processResponse);
+
 
 // next steps
 // https://console.bluemix.net/docs/services/conversation/develop-app.html#construindo-um-aplicativo-cliente
